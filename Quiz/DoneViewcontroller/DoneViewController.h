a2871d4bca220a80d7c153e11b2d79e42d007430
Pablo Alejandro Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-08-27 08:53:19 +0100

    Added "Done controller", which will be showed once the quiz is completed

---
+++ Quiz/DoneViewcontroller/DoneViewController.h
@@ -0,0 +1,16 @@
+//
+//  DoneViewController.h
+//  Quiz
+//
+//  Created by Pablo Alejandro Perez Martinez on 26/08/2016.
+//  Copyright © 2016 Pau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoneViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UILabel * doneLabel;
+@property (nonatomic, weak) IBOutlet UILabel * pointsLabel;
+
+@end
---
+++ Quiz/DoneViewcontroller/DoneViewController.m
@@ -0,0 +1,13 @@
+//
+//  DoneViewController.m
+//  Quiz
+//
+//  Created by Pablo Alejandro Perez Martinez on 26/08/2016.
+//  Copyright © 2016 Pau. All rights reserved.
+//
+
+#import "DoneViewController.h"
+
+@implementation DoneViewController
+
+@end
