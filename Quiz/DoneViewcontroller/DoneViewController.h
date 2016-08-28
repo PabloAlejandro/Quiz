e07305cd79e897b830f95265c09779ddfec8cf94
Pablo Alejandro Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-08-28 09:49:20 +0100

    View controller showed when quiz is finished
    It allows the debug version to restart test

--- Quiz/DoneViewcontroller/DoneViewController.h
+++ Quiz/DoneViewcontroller/DoneViewController.h
@@ -8,9 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
+@class QuizController;
+
 @interface DoneViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UIButton * restartButton;
 @property (nonatomic, weak) IBOutlet UILabel * doneLabel;
 @property (nonatomic, weak) IBOutlet UILabel * pointsLabel;
+
+@property (nonatomic, strong) QuizController * quizController;
 
 @end
--- Quiz/DoneViewcontroller/DoneViewController.m
+++ Quiz/DoneViewcontroller/DoneViewController.m
@@ -7,7 +7,63 @@
 //
 
 #import "DoneViewController.h"
+#import "QuizController.h"
+#import "AppDelegate.h"
+
+@interface DoneViewController ()
+
+@end
 
 @implementation DoneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+#ifdef DEBUG
+    self.restartButton.hidden = false;
+    self.restartButton.enabled = true;
+#else
+    self.restartButton.hidden = true;
+    self.restartButton.enabled = false;
+#endif
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+
+    [super viewWillAppear:animated];
+    
+    self.doneLabel.text = @"You have already done this quiz";
+    self.pointsLabel.text = [NSString stringWithFormat:@"You got %lu points!", [self.quizController totalPoints]];
+}
+
+#pragma mark - Private
+
+- (void)restartQuiz
+{
+    [self.quizController restart];
+    self.quizController = nil;
+    
+    UIViewController *viewController = [self.storyboard instantiateInitialViewController];
+    AppDelegate * delegate = [[UIApplication sharedApplication] delegate];
+    delegate.window.rootViewController = viewController;
+}
+
+#pragma mark - IBAction methods
+
+- (IBAction)restaryQuiz:(id)sender {
+    [self restartQuiz];
+}
+
+#pragma mark - Getters
+
+- (QuizController *)quizController {
+    
+    if(!_quizController) {
+        AppDelegate * delegate = [[UIApplication sharedApplication] delegate];
+        _quizController = delegate.quizController;
+    }
+    return _quizController;
+}
 
 @end
