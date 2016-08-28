e5be718b2958a9d7bed54f5fafdf0534782dd8b5
Pablo Alejandro Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-08-28 09:45:22 +0100

    Quiz controller added

--- Utils/QuizController/QuizController.h
+++ Utils/QuizController/QuizController.h
@@ -8,19 +8,49 @@
 
 #import <Foundation/Foundation.h>
 
-@interface QuizController : NSObject
+@interface QuizController : NSObject //<NSCoding>
 
+// init method not available
 - (instancetype)init  __attribute__((unavailable("Must use initWithFileName: instead.")));
+
+// Designated initializer
 - (instancetype)initWithFileName:(NSString *)fileName NS_DESIGNATED_INITIALIZER;
+
+// Method to returns a list of urls as strings
 - (NSArray <NSString *> *)imagesForQuestion:(NSString *)question;
+
+// Method to check if a response is correct
 - (BOOL)responseIsCorrect:(NSString *)response question:(NSString *)question;
-- (void)markAsDone:(BOOL)done;
-- (void)setPoint:(NSInteger)point forQuestion:(NSString *)question;
+
+// Get correct response for a question
+- (NSString *)correctResponseForQuestion:(NSString *)question;
+
+// Get user's last response for question
+- (NSString *)userResponseForQuestion:(NSString *)question;
+
+// Method to save user's response
+- (BOOL)addResponse:(NSString *)response toQuestion:(NSString *)question;;
+
+// Method to set the starting date
+- (void)start;
+
+// DEBUG: method to restart quiz and start over again
+- (void)restart;
+
+// Current points for the quiz
 - (NSInteger)totalPoints;
-- (BOOL)quizAlreadyDone;
 
-+ (BOOL)quizAlreadyDone:(NSString *)quizName;
+// Method to check if the user has already done the quiz
+- (BOOL)finished;
 
+// Method to save current state
+- (void)saveState;
+
+@property (nonatomic, copy, readonly) NSString * fileName;
 @property (nonatomic, strong, readonly) NSArray <NSDictionary *> * questions;
+@property (nonatomic, strong, readonly) NSDate * startDate;
+@property (nonatomic, assign, readonly) NSUInteger currentIndex;
+@property (nonatomic, assign, readonly) NSTimeInterval maxTimeInterval;
+@property (nonatomic, assign, readonly, getter=isStarted) BOOL started;
 
 @end
--- Utils/QuizController/QuizController.m
+++ Utils/QuizController/QuizController.m
@@ -7,29 +7,90 @@
 //
 
 #import "QuizController.h"
-#import "NSFileManager+Plist.h"
+#import "NSFileManager+Json.h"
 #import "ApplicationStateUtils.h"
+
+#define kContentKey                 @"Content"
+#define kFileNameKey                @"FileName"
+#define kResponsesKey               @"Responses"
+#define kQuestionsKey               @"Questions"
+#define kStartDateKey               @"StartDate"
+
+static NSTimeInterval const maxTimeInterval = 2 * 60;
 
 @interface QuizController ()
 
-@property (nonatomic, strong, readonly) NSDictionary * content;
-@property (nonatomic, copy, readonly) NSString * fileName;
+@property (nonatomic, strong, readwrite) NSDate * startDate;
+@property (nonatomic, copy, readwrite) NSString * fileName;
+@property (nonatomic, strong, readwrite) NSDictionary * content;
+@property (nonatomic, strong) NSMutableDictionary * responses;
 
 @end
 
 @implementation QuizController
 
 - (instancetype)initWithFileName:(NSString *)fileName {
+    
     if(self = [super init]) {
+        
         _fileName = fileName;
-        _content = [[NSFileManager defaultManager] readJson:fileName];
-        _questions = [[_content allKeys] copy]; // make dictionary with key and results
+//        _content = [[NSFileManager defaultManager] readJson:fileName];
+//        NSMutableArray * questions = [NSMutableArray new];
+//        for(NSString * key in [_content allKeys]) {
+//            NSDictionary * dict = @{
+//                                    @"question" : key,
+//                                    @"responses" : [_content objectForKey:key] ? :[NSNull null]
+//                                    };
+//            [questions addObject:dict];
+//        }
+//        _questions = [questions copy];
+//        _responses = [NSMutableDictionary new];
+        
+        // Get data from last session
+        NSDictionary * info = [ApplicationStateUtils getObjectProperty:fileName orDefault:nil];
+        
+        if(info) {
+            
+            if([[info objectForKey:kContentKey] isKindOfClass:[NSDictionary class]])
+                _content = [info objectForKey:kContentKey];
+            
+            if([[info objectForKey:kQuestionsKey] isKindOfClass:[NSArray class]])
+                _questions = [info objectForKey:kQuestionsKey];
+            
+            if([[info objectForKey:kResponsesKey] isKindOfClass:[NSDictionary class]])
+                _responses = [[NSMutableDictionary alloc] initWithDictionary:[info objectForKey:kResponsesKey]];
+            
+            if([[info objectForKey:kStartDateKey] isKindOfClass:[NSDate class]])
+                _startDate = [info objectForKey:kStartDateKey];
+        }
+        
+        // If there wasn't any data we initialiase it
+        if(!_content)
+            _content = [[NSFileManager defaultManager] readJson:fileName];
+        
+        if(!_questions) {
+            NSMutableArray * questions = [NSMutableArray new];
+            for(NSString * key in [_content allKeys]) {
+                NSDictionary * dict = @{
+                                        @"question" : key,
+                                        @"responses" : [_content objectForKey:key] ? :[NSNull null]
+                                        };
+                [questions addObject:dict];
+            }
+            _questions = [questions copy];
+        }
+        
+        if(!_responses)
+            _responses = [NSMutableDictionary new];
     }
     return self;
 }
 
 #pragma mark - Private
 
++ (NSString *)finishedKeyForFile:(NSString *)fileName {
+    return [NSString stringWithFormat:@"%@_finished", fileName];
+}
 
 #pragma mark - Public instance methods
 
@@ -38,50 +99,93 @@
     return [self.content objectForKey:question];
 }
 
-- (NSString *)responseForQuestion:(NSString *)question
+- (NSString *)correctResponseForQuestion:(NSString *)question
 {
     return [[self.content objectForKey:question] firstObject];
 }
 
 - (BOOL)responseIsCorrect:(NSString *)response question:(NSString *)question
 {
-    return [[self responseForQuestion:question] isEqualToString:response];
+    if(!response || !question) {return false;}
+    
+    return [[self correctResponseForQuestion:question] isEqualToString:response];
 }
 
-- (void)markAsDone:(BOOL)done
+- (NSString *)userResponseForQuestion:(NSString *)question;
 {
-    [ApplicationStateUtils setBoolProperty:self.fileName value:done];
+    return [self.responses objectForKey:question];
 }
 
-- (void)setPoint:(NSInteger)point forQuestion:(NSString *)question
+- (BOOL)addResponse:(NSString *)response toQuestion:(NSString *)question
 {
-    [ApplicationStateUtils setIntProperty:self.fileName value:point];
+    if(!response || !question) {return false;}
+    [self.responses setObject:response forKey:question];
+    return true;
+}
+
+- (void)start
+{
+    if(![self isStarted]) {
+        self.startDate = [NSDate date];
+    }
+}
+
+- (void)restart
+{
+    self.startDate = nil;
+    self.responses = [NSMutableDictionary new];
+    
+    [ApplicationStateUtils setObjectProperty:self.fileName value:nil];
 }
 
 - (NSInteger)totalPoints
 {
     NSInteger points = 0;
-    for(NSString * question in self.questions) {
-        points += [ApplicationStateUtils getIntProperty:question orDefault:0];
+    for(NSString * question in [self.responses allKeys]) {
+        NSString * response = [self.responses objectForKey:question];
+        NSUInteger point = [self responseIsCorrect:response question:question] ? 1 : 0;
+        points += point;
     }
     return points;
 }
 
-- (BOOL)quizAlreadyDone
+- (BOOL)finished
 {
-    return [[self class] quizAlreadyDone:self.fileName];
+    return fabs([self.startDate timeIntervalSinceNow]) >= self.maxTimeInterval ||
+    self.responses.count == self.questions.count;
 }
 
-#pragma mark - Public class methods
+#pragma mark - Getters
 
-+ (BOOL)quizAlreadyDone:(NSString *)quizName
-{
-#ifdef DEBUG
-    // TODO: Restore result
-    return false;
-#endif
+- (BOOL)isStarted {
+    return self.startDate != nil;
+}
+
+- (NSUInteger)currentIndex {
+    return [self.responses allKeys].count;
+}
+
+- (NSTimeInterval)maxTimeInterval {
+    return maxTimeInterval;
+}
+
+- (void)saveState {
     
-    return [ApplicationStateUtils getBoolProperty:quizName orDefault:false];
+    NSMutableDictionary * info = [NSMutableDictionary new];
+    
+    if(self.content)
+        [info setObject:self.content forKey:kContentKey];
+    
+    if(self.questions)
+        [info setObject:self.questions forKey:kQuestionsKey];
+    
+    if(self.responses)
+        [info setObject:self.responses forKey:kResponsesKey];
+    
+    if(self.startDate)
+        [info setObject:self.startDate forKey:kStartDateKey];
+    
+    [ApplicationStateUtils setObjectProperty:self.fileName value:info];
 }
 
 @end
