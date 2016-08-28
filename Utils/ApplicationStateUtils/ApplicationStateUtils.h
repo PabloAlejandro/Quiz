dc5196ef9c98877d5e2c780f2fa274cf3973ad1a
Pablo Alejandro Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-08-28 09:46:57 +0100

    Application state utils simplified just supporting ibjects

--- Utils/ApplicationStateUtils/ApplicationStateUtils.h
+++ Utils/ApplicationStateUtils/ApplicationStateUtils.h
@@ -13,25 +13,7 @@
  */
 @interface ApplicationStateUtils : NSObject
 
-+ (BOOL)getBoolProperty:(const NSString *)propertyName orDefault:(BOOL)defaultValue;
-+ (void)setBoolProperty:(const NSString *)propertyName value:(BOOL)value;
-
-+ (int)getIntProperty:(const NSString *)propertyName orDefault:(NSInteger)defaultValue;
-+ (void)setIntProperty:(const NSString *)propertyName value:(NSInteger)value;
-
-+ (float)getFloatProperty:(const NSString *)propertyName orDefault:(float)defaultValue;
-+ (void)setFloatProperty:(const NSString *)propertyName value:(float)value;
-
-+ (double)getDoubleProperty:(const NSString *)propertyName orDefault:(float)defaultValue;
-+ (void)setDoubleProperty:(const NSString *)propertyName value:(double)value;
-
-+ (NSString *)getStringProperty:(const NSString *)propertyName orDefault:(NSString *)defaultValue;
-+ (void)setStringProperty:(const NSString *)propertyName value:(NSString *)value;
-
-+ (NSDate *)getDateProperty:(const NSString *)propertyName orDefault:(NSDate *)defaultValue;
-+ (void)setDateProperty:(const NSString *)propertyName value:(NSDate *)value;
-
-// Note: Despite we just need two of these methods, it is good to provide the whole
-// suit of methods in case we need some of them in the future.
++ (id)getObjectProperty:(const NSString *)propertyName orDefault:(id)defaultValue;
++ (void)setObjectProperty:(const NSString *)propertyName value:(id)value;
 
 @end
--- Utils/ApplicationStateUtils/ApplicationStateUtils.m
+++ Utils/ApplicationStateUtils/ApplicationStateUtils.m
@@ -10,85 +10,14 @@
 
 @implementation ApplicationStateUtils
 
-+ (BOOL)getBoolProperty:(const NSString *)propertyName orDefault:(BOOL)defaultValue {
++ (id)getObjectProperty:(const NSString *)propertyName orDefault:(id)defaultValue {
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     id property = [defaults objectForKey:(NSString *)propertyName];
-    return property != nil ? [property boolValue] : defaultValue;
-}
-
-+ (void)setBoolProperty:(const NSString *)propertyName value:(BOOL)value {
-
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setBool:value forKey:(NSString *)propertyName];
-    [defaults synchronize];
-}
-
-+ (int)getIntProperty:(const NSString *)propertyName orDefault:(NSInteger)defaultValue {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    id property = [defaults objectForKey:(NSString *)propertyName];
-    return property != nil ? [property intValue] : (int)defaultValue;
-}
-
-+ (void)setIntProperty:(const NSString *)propertyName value:(NSInteger)value {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setInteger:value forKey:(NSString *)propertyName];
-    [defaults synchronize];
-}
-
-
-+ (float)getFloatProperty:(const NSString *)propertyName orDefault:(float)defaultValue {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    id property = [defaults objectForKey:(NSString *)propertyName];
-    return property != nil ? [property floatValue] : defaultValue;
-}
-
-+ (void)setFloatProperty:(const NSString *)propertyName value:(float)value {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setFloat:value forKey:(NSString *)propertyName];
-    [defaults synchronize];
-}
-
-+ (double)getDoubleProperty:(const NSString *)propertyName orDefault:(float)defaultValue {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    id property = [defaults objectForKey:(NSString *)propertyName];
-    return property != nil ? [property floatValue] : defaultValue;
-}
-
-+ (void)setDoubleProperty:(const NSString *)propertyName value:(double)value {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setFloat:value forKey:(NSString *)propertyName];
-    [defaults synchronize];
-}
-
-+ (NSString *)getStringProperty:(const NSString *)propertyName orDefault:(NSString *)defaultValue {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSString *property = [defaults objectForKey:(NSString *)propertyName];
     return property != nil ? property : defaultValue;
 }
 
-+ (void)setStringProperty:(const NSString *)propertyName value:(NSString *)value {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setValue:value forKey:(NSString *)propertyName];
-    [defaults synchronize];
-}
-
-+ (NSDate *)getDateProperty:(const NSString *)propertyName orDefault:(NSDate *)defaultValue {
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSDate *property = [defaults objectForKey:(NSString *)propertyName];
-    return property != nil ? property : defaultValue;
-}
-
-+ (void)setDateProperty:(const NSString *)propertyName value:(NSDate *)value {
++ (void)setObjectProperty:(const NSString *)propertyName value:(id)value {
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setValue:value forKey:(NSString *)propertyName];
